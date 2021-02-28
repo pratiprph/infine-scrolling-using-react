74d5d4123bd6db47d6cd19391f42736ec6a61a96
pratiprph <pratipsinha@gmail.com>
2021-02-28 20:14:17 +0530

    search-feture added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const [photos, setPhotos] = useState([])
 const [start, setStart ] = useState(0)
 const [end, setEnd ] = useState(5)
 const [ tag, setTag ] = useState('')
+const [ search, setSearch ] = useState('')
 
  useEffect(()=>{
   fetch('https://jsonplaceholder.typicode.com/photos')
@@ -25,25 +26,49 @@ const [ tag, setTag ] = useState('')
   }
 };
 
+function searchItem(event){
+  let search_str = event.target.value
+
+  let photo = photos.filter((el)=>el.title.includes(search_str))
+  console.log(photo)
+  console.log(search_str)
+  if(search_str===" "){
+    setPhotos(photos)
+  } else {
+    setPhotos(photo)
+  }
+  
+}
+
   return (
-    <div classNameName="App">
+    <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      
+      <h2>Start typing to search!</h2>
+       <input type="text" style={{"width":"500px","borderRadius":"10px"}}
+       onChange={searchItem}/><br/><br/>
+
+      <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Album ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Icon</th>
+        </tr>
+      </thead>
+      <tbody>
        {
          photos.splice(start,end).map((el)=>(
-          <div className="card" style={{"width": "18rem"}}>
-          <img className="card-img-top" src={el.thumbnailUrl} alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">{el.title}</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div><hr/>
-        </div>
+          <tr key={el.id}>
+            <th scope="row">1</th>
+            <td>{el.id}</td>
+            <td>{el.title}</td>
+            <td><img src={el.thumbnailUrl} style={{"width":'100px',"height":"70px"}}/></td>
+          </tr>
         
          ))
        }
-      
+       </tbody>
+      </table>
     </div>
   );
 }
